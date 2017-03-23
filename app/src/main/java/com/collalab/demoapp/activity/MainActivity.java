4fc0e37a2b5922645e89662e0c36404ecb6e9d1e
Viet <viet@mobilefolk.com>
2017-03-23 18:16:48 +0700

    Add dispatcher activity

--- app/src/main/java/com/collalab/demoapp/activity/MainActivity.java
+++ app/src/main/java/com/collalab/demoapp/activity/MainActivity.java
@@ -40,7 +40,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
         PreferenceUtils.init(this);
---
+++ app/src/main/java/com/collalab/demoapp/activity/SonHaDispatcherActivity.java
@@ -0,0 +1,27 @@
+package com.collalab.demoapp.activity;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.collalab.demoapp.persistence.PreferenceUtils;
+import com.collalab.demoapp.persistence.PrefsKey;
+
+public class SonHaDispatcherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        PreferenceUtils.init(this);
+
+        boolean isLoggedIn = PreferenceUtils.getBoolean(PrefsKey.KEY_IS_LOGGED_IN,false);
+        Intent intent;
+        if(isLoggedIn) {
+            intent = new Intent(SonHaDispatcherActivity.this,TabActivity.class);
+        } else {
+            intent = new Intent(SonHaDispatcherActivity.this,MainActivity.class);
+        }
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
+}
